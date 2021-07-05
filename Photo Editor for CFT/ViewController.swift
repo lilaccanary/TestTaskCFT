dfbb77edd552aac5d04b412d8e2f04206167f0ad
Alina Kvashnina <lilaccanary@gmail.com>
2021-07-05 15:21:37 +0700

    Initial Commit

---
+++ Photo Editor for CFT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photo Editor for CFT
+//
+//  Created by Алина Квашнина on 05.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
